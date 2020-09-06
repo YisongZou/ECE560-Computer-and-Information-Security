0ce081bc4cc2b57ab436445771295adc9aefc3c0
yiongzou <yisong.zou@duke.edu>
2020-09-05 20:21:29 -0400

    Finish hw1

---
+++ ECE560_Security/hw1/getbanner.cpp
@@ -0,0 +1,49 @@
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+using namespace std;
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    cout << "Syntax: ./getbanner <hostname> <port_num>\n" << endl;
+    return 1;
+  }
+  int status;
+  int socket_fd;
+  struct addrinfo host_info;
+  struct addrinfo *host_info_list;
+  const char *hostname = argv[1];
+  const char *port = argv[2];
+  memset(&host_info, 0, sizeof(host_info));
+  host_info.ai_family = AF_UNSPEC;
+  host_info.ai_socktype = SOCK_STREAM;
+  status = getaddrinfo(hostname, port, &host_info, &host_info_list);
+  if (status != 0) {
+    cerr << "Error: cannot get address info for host" << endl;
+    cerr << "  (" << hostname << "," << port << ")" << endl;
+    return -1;
+  }
+  socket_fd = socket(host_info_list->ai_family, host_info_list->ai_socktype,
+                     host_info_list->ai_protocol);
+  if (socket_fd == -1) {
+    cerr << "Error: cannot create socket" << endl;
+    cerr << "  (" << hostname << "," << port << ")" << endl;
+    return -1;
+  }
+  status =
+      connect(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
+  if (status == -1) {
+    cerr << "Error: cannot connect to socket" << endl;
+    cerr << "  (" << hostname << "," << port << ")" << endl;
+    return -1;
+  }
+  const char *message = "QUIT\n";
+  send(socket_fd, message, strlen(message), 0);
+  char buffer[512];
+  recv(socket_fd, buffer, 512, 0);
+  cout << buffer;
+  freeaddrinfo(host_info_list);
+  close(socket_fd);
+  return 0;
+}
